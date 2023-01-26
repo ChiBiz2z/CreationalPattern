6f43bddbdcd575341e268a1e9e7ac29ac0bea394
Vladimir Bedash <vvbedash@gmail.com>
2023-01-26 15:05:35 +0600

    Inital Maze

---
+++ Maze/Maze.cs
@@ -0,0 +1,29 @@
+﻿namespace Factory;
+
+/// <summary>
+/// Класс Maze используется для представления лабиринта, как набора комнат.
+/// </summary>
+public class Maze
+{
+    private Dictionary<int, Room> rooms;
+
+    public Maze()
+    {
+        rooms = new Dictionary<int, Room>();
+    }
+
+    public void AddRoom(Room room)
+    {
+        rooms.Add(room.RoomNumber, room);
+    }
+    
+    /// <summary>
+    /// Для получения ссылки на экземпляр комнаты по ее номеру
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    public Room GetRoom(int number)
+    {
+        return rooms[number];
+    }
+}
---
+++ Maze/MazeGame.cs
@@ -0,0 +1,39 @@
+﻿
+using Factory.Enums;
+
+namespace Factory;
+
+public class MazeGame
+{
+    /// <summary>
+    /// Cоздает лабиринт состоящий из двух комнат с одной дверью между ними и возвращает ссылку на экземпляр созданного лабиринта.
+    /// </summary>
+    /// <returns>Лабиринт</returns>
+    public Maze CreateMaze()
+    {
+        // лабиринт
+        var maze = new Maze();
+        
+        // две комнаты между ними
+        var r1 = new Room(1);
+        var r2 = new Room(2);
+        
+        // дверь между двумя комнатами
+        var door = new Door(r1, r2);
+        
+        maze.AddRoom(r1);
+        maze.AddRoom(r2);
+        
+        r1.SetSide(Direction.North, new Wall());
+        r1.SetSide(Direction.East, door);
+        r1.SetSide(Direction.South, new Wall());
+        r1.SetSide(Direction.West, new Wall());
+        
+        r2.SetSide(Direction.North, new Wall());
+        r2.SetSide(Direction.East, new Wall());
+        r2.SetSide(Direction.South, new Wall());
+        r2.SetSide(Direction.West, door);
+
+        return maze;
+    }
+}
